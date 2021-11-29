c9e643837237c3c05348b84c175a14b00608df83
Nathan Smith <nsmith@lcs.on.ca>
2021-11-29 14:25:42 -0500

    Initial Commit

---
+++ DiningHall/DiningHallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiningHallApp.swift
+//  DiningHall
+//
+//  Created by Nate S on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct DiningHallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
